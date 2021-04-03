1c4ac207ad2148a3030ae1e9903802264c2e16a6
lohitagarwalla <lohitagarwalla@gmail.com>
2021-04-03 22:35:55 +0530

    Fix for heroku server
    Signed-off-by: lohitagarwalla <lohitagarwalla@gmail.com>

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
- mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-     useNewUrlParser: true,
-     useCreateIndex: true,
-     useUnifiedTopology: true
- } )
+//  mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
 
 
 // const task = new Task({
